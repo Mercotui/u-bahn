eee1a4a992fd3fcf9c64193074aacd9e62752c5c
Menno van der Graaf <mennovandergraaf@hotmail.com>
2024-07-23 01:42:31 +0200

    Set a custom logging callback for raylib to use absl logger

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <absl/log/globals.h>
 #include <absl/log/initialize.h>
+#include <absl/log/log.h>
 #include <raylib.h>
 #if defined(PLATFORM_WEB)
 #include <emscripten/emscripten.h>
@@ -10,13 +11,41 @@
 namespace {
 constexpr auto kInitialScreenWidth = 1300;
 constexpr auto kInitialScreenHeight = 900;
+
+absl::LogSeverity RaylibToAbslLogLevel(TraceLogLevel raylib_log_level) {
+  switch (raylib_log_level) {
+    case LOG_DEBUG:
+      // absl doesn't go below info level unlike every other log library for some reason
+      [[fallthrough]];
+    case LOG_INFO:
+      return absl::LogSeverity::kInfo;
+    case LOG_WARNING:
+      return absl::LogSeverity::kWarning;
+    case LOG_ERROR:
+      return absl::LogSeverity::kError;
+    case LOG_FATAL:
+      return absl::LogSeverity::kFatal;
+    default:
+      // failsafe I guess, if log level is unknown then we treat it as an error.
+      return absl::LogSeverity::kError;
+  }
+}
 }  // namespace
 
 int main() {
   absl::InitializeLog();
   absl::SetStderrThreshold(absl::LogSeverityAtLeast::kInfo);
 
-  SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_WINDOW_HIGHDPI);
+  // divert raylib logging into absl logging
+  SetTraceLogCallback(+[](int level, const char* text, va_list args) {
+    constexpr auto kMaxLogLength = 2048;
+    char expanded_text[kMaxLogLength];
+    std::vsnprintf(expanded_text, kMaxLogLength, text, args);
+    auto absl_level = RaylibToAbslLogLevel(static_cast<TraceLogLevel>(level));
+    LOG(LEVEL(absl_level)) << "raylib: " << expanded_text;
+  });
+
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_WINDOW_HIGHDPI | FLAG_MSAA_4X_HINT);
   InitWindow(kInitialScreenWidth, kInitialScreenHeight, "U-Bahn");
 
   Game game{};
